d619b697734ee59342fdee14d0be7915d9cd7976
chaudhuree <chaudhuree00@gmail.com>
2022-06-06 23:58:30 +0600

    docs: project section completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Acomplishments from '../components/Acomplishments/Acomplishments';
 import BgAnimation from '../components/BackgrooundAnimation/BackgroundAnimation';
 import Hero from '../components/Hero/Hero';
@@ -6,10 +7,13 @@ import Technologies from '../components/Technologies/Technologies';
 import Timeline from '../components/TimeLine/TimeLine';
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
-
 const Home = () => {
   return (
+   
     <Layout>
+     <Head>
+      <title>Blackweaver</title>
+    </Head>
       <Section grid>
         <Hero />
         <BgAnimation />
